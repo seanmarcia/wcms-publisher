b682591329088d475c696eefc5ac831f34ab2d0b
Ryan Hall <ryan.hall@biola.edu>
2015-04-22 15:27:11 -0700

    updating and configuring buweb

---
+++ config/initializers/buweb_content_models.rb
@@ -0,0 +1,7 @@
+BuwebContentModels.configure do |config|
+  config.urn_namespaces = Settings.urn_namespaces
+  config.roz_api_base_url = Settings.roz.api_base_url
+  config.roz_files_base_url = Settings.roz.files_base_url
+  config.roz_access_id = Settings.roz.access_id
+  config.roz_secret_key = Settings.roz.secret_key
+end

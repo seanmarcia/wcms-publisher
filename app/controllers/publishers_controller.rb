3dcf6766f07d6cbd37153f36d0e147f898142406
Ryan Hall <ryan.hall@biola.edu>
2015-04-28 14:43:48 -0700

    Adding more to the gitignore file and fixing bug if there are no publishers.

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -4,7 +4,7 @@ class PublishersController < ApplicationController
   skip_after_action :verify_policy_scoped, only: :index
 
   def index
-    @publishers = Settings.publishers
+    @publishers = Array(Settings.publishers)
   end
 
 end

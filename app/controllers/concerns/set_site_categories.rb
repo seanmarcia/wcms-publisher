74ccd1ead409a03b53277aa595d1cbd56581c55e
Ryan Hall <ryan.hall@biola.edu>
2015-07-21 15:39:12 -0700

    We don't need to humanize the title.

--- app/controllers/concerns/set_site_categories.rb
+++ app/controllers/concerns/set_site_categories.rb
@@ -5,7 +5,7 @@ module SetSiteCategories
     @sites = Site.where(has_page_editions: true)
     @site_categories = {}
     @sites.each do |site|
-      @site_categories[site.id] = site.categories(type).map{ |cat| { title: cat.title.humanize, id: cat.id.to_s } }
+      @site_categories[site.id] = site.categories(type).map{ |cat| { title: cat.title, id: cat.id.to_s } }
     end
   end
 end

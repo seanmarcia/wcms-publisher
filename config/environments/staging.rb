36e5ba0a7a430c7bfff901d600134f4ba7847ff4
Ryan Hall <ryan.hall@biola.edu>
2015-11-24 16:48:01 -0800

    Updating text for new Media link and fixing deprecation warning.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -14,8 +14,9 @@ Wcms::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true

6b0d341ff9236345da394eb7087e5a0e26b1272c
Ryan Hall <ryan.hall@biola.edu>
2016-01-15 16:09:06 -0800

    Updating permissions so a page author has permissions to edit the page they create. WCMS-612

--- app/controllers/page_editions_controller.rb
+++ app/controllers/page_editions_controller.rb
@@ -122,7 +122,7 @@ class PageEditionsController < ApplicationController
   # On create if an author doesnt already have permissions to edit create a permission
   #  allowing them to edit this page.
   def set_author
-    unless policy(@page_edition).page_editor?
+    unless policy(@page_edition).edit?
       author = Permission.new(
                       actor_id: current_user.id,
                       actor_type: 'User',

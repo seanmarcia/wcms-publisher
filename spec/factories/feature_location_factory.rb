4c85f28f92848fb1db8174a545e1366fc7d9d5d8
Ryan Hall <ryan.hall@biola.edu>
2016-03-01 15:12:02 -0800

    Loading factories from buweb instead of restating them all

--- spec/factories/feature_location_factory.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :feature_location do
-    sequence(:title) { |n| "Title ##{n}" }
-    sequence(:path) { |n| "/path/to/#{n}"}
-    sequence(:slug) { |n| "slug-#{n}"}
-
-    site
-  end
-end
--- spec/factories/menu_factory.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :menu do
-    title 'selection aiding platform'
-
-  end
-end

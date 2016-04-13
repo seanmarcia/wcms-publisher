f57d705dd69ce2a2e0543dfaabd06d64c9fcf0d9
Ryan Hall <ryan.hall@biola.edu>
2016-04-12 17:42:41 -0700

    Fixing photo section of events form

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -3,18 +3,4 @@ module FormHelper
     options[:builder] = PermittedFormBuilder
     form_for(object, options, &block)
   end
-
-  def divider(text=nil)
-    content_tag :div, class: 'divider' do
-      content_tag(:div, '', class: 'line') +
-      if text.present?
-        content_tag(:div, class: 'box_wrapper') do
-          content_tag(:div, text, class: 'box')
-        end
-      else
-        ''
-      end
-    end
-  end
-
 end

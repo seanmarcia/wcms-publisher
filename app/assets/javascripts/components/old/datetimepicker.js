aa377c047ead4781c60b6a47526d36f88523cb44
Ryan Hall <ryan.hall@biola.edu>
2016-04-27 14:29:10 -0700

    Small changes

--- app/assets/javascripts/components/old/datetimepicker.js
+++ app/assets/javascripts/components/old/datetimepicker.js
@@ -6,11 +6,16 @@ resetDatetimePicker = function (object) {
       useCurrent: false,
       sideBySide: true,
       icons: {
-        time: "fa fa-clock-o",
-        date: "fa fa-calendar",
-        up: "fa fa-arrow-up",
-        down: "fa fa-arrow-down"
-      }
+        time: 'fa fa-clock-o',
+        date: 'fa fa-calendar',
+        up: 'fa fa-chevron-up',
+        down: 'fa fa-chevron-down',
+        previous: 'fa fa-chevron-left',
+        next: 'fa fa-chevron-right',
+        today: 'fa fa-dot-circle-o',
+        clear: 'fa fa-trash',
+        close: 'fa fa-times'
+      },
     })
   }
 }

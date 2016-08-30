f5c1c3a1a527eb85a7fabde4a392a921a05f0a79
Jeff Silzer <jeff.silzer@biola.edu>
2016-08-29 23:18:54 -0700

    add links to staging and production

--- app/helpers/status_helper.rb
+++ app/helpers/status_helper.rb
@@ -1,12 +1,16 @@
 module StatusHelper
   def status_for(item)
-    label_class = case item.aasm_state
+    label_class = class_partial_for(item.aasm_state)
+
+    content_tag :span, item.aasm_state.titleize, class: "label label-#{label_class}"
+  end
+
+  def class_partial_for(state)
+    case state
     when 'published' then 'success'
     when 'archived' then 'warning'
     when 'ready_for_review' then 'info'
     else 'default'
     end
-
-    content_tag :span, item.aasm_state.titleize, class: "label label-#{label_class}"
   end
 end

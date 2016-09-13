5652a3ba8875397c9c7a7dc1309265f4577e64c8
Irene Koan <irene.koan@biola.edu>
2016-09-13 16:14:23 -0700

    Add search feature to presentation data templates

---
+++ app/extensions/presentation_data_template.rb
@@ -0,0 +1,12 @@
+PresentationDataTemplate.class_eval do
+  scope :custom_search, -> query {
+    begin
+      if query.present?
+        q = Regexp.new(query.to_s, Regexp::IGNORECASE)
+        any_of({title: q}, {target_class: q})
+      end
+    rescue RegexpError
+      PresentationDataTemplate.none
+    end
+  }
+end

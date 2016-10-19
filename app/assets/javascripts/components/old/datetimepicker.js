ec48fda49895078102bdb886f9ffa1e192d3fd07
Devin Hanlon <devin.hanlon@biola.edu>
2016-10-19 13:04:22 -0700

    State dates (#117)
    * Added date/time picker to state dates for applicable classes
    
    * showing clock and added hide article option
    
    * Updating to use datetime_select method and existing JS code for datetime-picker
    
    * Add support for linking date time fields

--- app/assets/javascripts/components/old/datetimepicker.js
+++ app/assets/javascripts/components/old/datetimepicker.js
@@ -1,5 +1,67 @@
-// Pass a jQuery object as the parameter
-// This should be available at the root scope
+//
+// To use the bootstrap-datetime picker field, just use one of
+// the rails FormBuilder helpers we created.
+// - date_select(method, options = {}, html_options = {})
+// - datetime_select(method, options = {}, html_options = {})
+//
+// Example:
+//   = f.datetime_select :field_name
+// or
+//   = f.date_select :field_name
+//
+//
+// `defineDateTimeRangeGroup` links two fields to enforce one is
+// always an earlier date than the other one.
+//
+// @param rangeGroups [jQueryObject]
+//   This should be the parent DOM element that contains both
+//   the `date-from` and the `date-to` elements
+//
+// Example usage:
+//  .date-time-range-group
+//    = f.datetime_select :publish_at, {}, class: 'date-from'
+//    = f.datetime_select :archive_at, {}, class: 'date-to'
+//
+defineDateTimeRangeGroup = function (rangeGroups) {
+  if (rangeGroups.length > 0) {
+    // Support multiple groups defined on the page
+    // Loop through each one.
+    rangeGroups.each(function(i, element) {
+      var fromDate = $(element).find('.date-from');
+      var toDate = $(element).find('.date-to');
+
+      // Skip if we can't find elements for both sides of the range
+      // inside the range group element.
+      if (fromDate.length == 1 && toDate.length == 1) {
+        // Initialize on page load
+        var initialFromDate = fromDate.find('input').val();
+        var initialToDate = toDate.find('input').val()
+        if (initialFromDate) {
+          toDate.data('DateTimePicker').minDate(moment(initialFromDate));
+        }
+        if (initialToDate) {
+          fromDate.data('DateTimePicker').maxDate(moment(initialToDate));
+        }
+
+        // Update whenever the field values change
+        fromDate.on('dp.change', function (e) {
+          toDate.data('DateTimePicker').minDate(e.date);
+        });
+        toDate.on('dp.change', function (e) {
+          fromDate.data('DateTimePicker').maxDate(e.date);
+        });
+      }
+    })
+  }
+}
+
+//
+// `resetDatetimePicker` initializes all the datetime pickers on the page.
+//
+// @param object [jQueryObject]
+//   This is the element you want to initialize
+// @public This function should be available globally
+//
 resetDatetimePicker = function (object) {
   if (object.length > 0) {
     object.datetimepicker({
@@ -22,4 +84,5 @@ resetDatetimePicker = function (object) {
 
 $(document).ready(function () {
   resetDatetimePicker($('.datetimepicker'));
+  defineDateTimeRangeGroup($('.date-time-range-group'));
 });
--- config/initializers/form_builder.rb
+++ config/initializers/form_builder.rb
@@ -5,7 +5,7 @@ module ActionView
       def date_select(method, options = {}, html_options = {})
         existing_date = @object.send(method)
         formatted_date = existing_date.to_date.strftime("%F") if existing_date.present?
-        @template.content_tag(:div, :class => "input-group datepicker") do
+        @template.content_tag(:div, :class => "input-group datepicker #{html_options[:class]}") do
           text_field(method, :value => formatted_date, :class => "form-control", :"data-date-format" => "YYYY-MM-DD", :placeholder => html_options[:placeholder]) +
           @template.content_tag(:span, @template.content_tag(:i, "", :class => "fa fa-calendar") ,:class => "input-group-addon")
         end
@@ -14,7 +14,7 @@ module ActionView
       def datetime_select(method, options = {}, html_options = {})
         existing_time = @object.send(method)
         formatted_time = existing_time.to_time.strftime("%F %I:%M %p") if existing_time.present?
-        @template.content_tag(:div, :class => "input-group datetimepicker") do
+        @template.content_tag(:div, :class => "input-group datetimepicker #{html_options[:class]}") do
           text_field(method, :value => formatted_time, :class => "form-control", :"data-date-format" => "YYYY-MM-DD hh:mm A", :placeholder => html_options[:placeholder]) +
           @template.content_tag(:span, @template.content_tag(:i, "", :class => "fa fa-calendar") ,:class => "input-group-addon")
         end

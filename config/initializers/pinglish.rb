f4099259ab14cdaf7d1d09d4b55b168ca0e4d419
Devin Hanlon <devin.hanlon@biola.edu>
2017-05-23 14:18:00 -0700

    Removed broken ping checks

--- config/initializers/pinglish.rb
+++ config/initializers/pinglish.rb
@@ -7,20 +7,7 @@ Wcms::Application.config.middleware.use Pinglish do |ping|
     Sidekiq.redis { |r| r.ping == 'PONG' }
   end
 
-  ping.check :ws do
-    BiolaWebServices.dirsvc.get_directories.map{|d| d['api'] }.compact.length > 0
-  end
-
   ping.check :elasticsearch do
     Elasticsearch::Model.client.indices.exists index: Settings.elasticsearch.index_name
-  end
-
-  ping.check :smtp do
-    smtp = Net::SMTP.new(ActionMailer::Base.smtp_settings[:address])
-    smtp.start
-    ok = smtp.started?
-    smtp.finish
-
-    ok
   end
 end
